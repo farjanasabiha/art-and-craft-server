57a8fbf50cd50a88b3c8a7721241f151fd595bd8
devsabiha <farjanasabiha786@gmail.com>
2024-08-19 16:14:53 +0600

    Re Check

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // MiddleWare
-
 app.use(cors());
 app.use(express.json());
 
@@ -14,7 +13,7 @@ console.log(process.env.DB_USER);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jtwnv2k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 console.log(uri);
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Create a MongoClient with a MongoClientOptions object to set the Stable API
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
